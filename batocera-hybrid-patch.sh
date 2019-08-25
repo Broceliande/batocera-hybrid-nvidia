f36443bfeb14250b1e4a404cf084b67111e05d26
Broceliande <eric.dolou@jcedeveloppement.com>
2019-08-25 18:26:14 +0200

    looks like batocera-settings fails if /boot is read-only

--- batocera-hybrid-patch.sh
+++ batocera-hybrid-patch.sh
@@ -4,6 +4,7 @@
 # tested on batocera 5.23 Acer Aspire vx15 with Nvidia GTX 1050TI gpu OPENGL String: 4.6.0 NVIDIA 430.40
 echo "Stopping ES..."
 /etc/init.d/S31emulationstation stop
+mount -o remount,rw /boot
 echo "Activating Nvidia driver"
 batocera-settings /boot/batocera-boot.conf --command uncomment --key nvidia-driver
 echo "Writing NVidia Xconf..."
@@ -24,5 +25,6 @@ cp /etc/X11/xinit/xinitrc /userdata/system/.xinitrc
 sed -i -e "s/## CUSTOMISATIONS ###/## CUSTOMISATIONS ###\n## FORCE NVIDIA - HYBRID PATCH ###\nxrandr --setprovideroutputsource modesetting NVIDIA-0\nxrandr --auto/g" /userdata/system/.xinitrc
 echo "Saving FS Overlay..."
 batocera-save-overlay
+mount -o remount,r /boot
 echo "Reboot now" 
 reboot
